22e0ffdc4c478ec5fd055ab91dd77ec81f672b9c
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-01 16:37:26 +0200

    Parent/Child Components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MyInfo from './components/MyInfo'
+import App from './App'
 
 
 
-ReactDOM.render(<MyInfo />, document.getElementById('root'))
+//ReactDOM.render(<MyInfo />, document.getElementById('root'))
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
