b1e9355f2b8561501c74b18ffa97c30c0136a38f
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-01 15:39:18 +0200

    create React app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let JSX =<ul>
+        <li>Milk</li>
+        <li>Bread</li>
+        <li>Nutela</li>
+        </ul>
+
+ReactDOM.render(JSX, document.getElementById('root'))
+
