580e34cafe0ade477a2e42835aef5a04cf9cecf1
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-02 09:53:18 +0200

    inline style and style property

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header (){
   return (
     <header>
-      <h3>This is my Navigator element</h3>
+      <h3 className="navbar">This is the header</h3>
     </header>
   )
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
 function MainContent(){
+
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfday
+  
+  const styles ={   
+    fontSize:24,
+    fontWeight: 'bold',
+    textTransform: 'capitalize',
+    fontStyle: 'italic'
+  }
+  if(hours < 12) {
+    timeOfday = 'morning'
+    styles.color = 'rgb(107, 153, 47)'
+  }
+  else if(hours >= 12 && hours < 17) {
+    timeOfday = 'afternoon'
+    styles.color = 'rgb(190, 165, 25)'
+  }
+  else {
+    timeOfday = 'night'
+    styles.color = 'rgb(100, 45, 8)'
+  }
+
+
   return (
   <main>
     <h1>Main Content</h1>
-    <p>nice try</p>
+    <p style={styles}>Good {timeOfday}</p>
     <ul>
       <li>football</li>
       <li>Volleyball</li>
