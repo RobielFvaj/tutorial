be75c568c244c81982c93f70697448820a396d73
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-02 10:49:08 +0200

    using props

---
+++ src/components/ContactCard.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import React from 'react'
+
+function ContactCard(props){
+
+  return (
+    <div className="contact-card">
+        <img src={props.contact.imgUrl} />
+        <h3>{props.contact.name}</h3>
+        <p>Phone: {props.contact.phone}</p>
+        <p>Email:{props.contact.email}</p>
+    </div>
+  )
+}
+
+export default ContactCard
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 function MainContent(){
 
@@ -30,11 +31,41 @@ function MainContent(){
   <main>
     <h1>Main Content</h1>
     <p style={styles}>Good {timeOfday}</p>
-    <ul>
-      <li>football</li>
-      <li>Volleyball</li>
-      <li>basketball</li>
-    </ul>
+    
+    <div className="contacts">
+      <ContactCard 
+         contact =  {{
+          name:"Mr. Whiskerson", 
+          imgUrl:"http://placekitten.com/300/200", 
+          phone:"(212) 555-1234", 
+          email:"mr.whiskaz@catnap.meow"}}
+      />
+      
+      <ContactCard 
+        contact =  {{
+          name:"Fluffykins", 
+          imgUrl:"http://placekitten.com/400/200", 
+          phone:"(212) 555-1234", 
+          email:"fluff@me.com"}}
+      />
+      
+      <ContactCard 
+        contact =  {{
+          name:"Destroyer", 
+          imgUrl:"http://placekitten.com/400/300", 
+          phone:"(212) 555-3456", 
+          email:"ofworlds@yahoo.com"}}
+      />
+      
+      <ContactCard 
+        contact =  {{
+          name:"Felix", 
+          imgUrl:"http://placekitten.com/200/100", 
+          phone:"(212) 555-4567", 
+          email:"thecat@hotmail.com"}}
+      />
+            
+        </div>
   </main>
   )
 }
