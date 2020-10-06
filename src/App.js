0c9eca7fad5802f67dda79520bba3125233de9e2
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-06 16:02:25 +0200

    separent le component

--- src/App.js
+++ src/App.js
@@ -10,36 +10,17 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      answer : 'Yes',
-      isLoading: true,
-      isLogging: true
+     
     }
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        isLoading: false
-      })
-    }, 1500)
-  }
-  handleClick (){
-    this.setState(prevState => ({isLogging : !prevState.isLogging}))
-}
+
 
   render(){
     return (
       <div>
          <Header />
-        <h1>Is state important to know? {this.state.answer}</h1>
-        {this.state.isLoading ? <h1>Loading ...</h1> :
-        <Conditional isLoading={this.state.isLoading} />}
-
-        <button onClick ={this.handleClick}>
-          {this.state.isLogging ? "Login" : "Logout"}
-        </button>
-        <h1>{this.state.isLogging ? "Logged out" : "Logged in"}</h1>
+         <Conditional isLoading={this.state.isLoading} />
          <MainContent />
          <Footer />
       </div>
--- src/Conditional.js
+++ src/Conditional.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
-function Conditional (props){
- return (
-    <h1>Temp</h1>
-  )
-  
+class Conditional extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      answer : 'Yes',
+      isLoading: true,
+      isLogging: true
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 1500)
+  }
+  handleClick (){
+    this.setState(prevState => ({isLogging : !prevState.isLogging}))
+}
+
+    render (props){
+    return (
+      <div>
+         <h1>Is state important to know? {this.state.answer}</h1>
+        {this.state.isLoading ? <h1>Loading ...</h1> : <h1>Temp</h1>}
+
+        <button onClick ={this.handleClick}>
+          {this.state.isLogging ? "Login" : "Logout"}
+        </button>
+        <h1>{this.state.isLogging ? "Logged out" : "Logged in"}</h1>
+        
+      </div>
+    )
+ }
 }
 
 export default Conditional
